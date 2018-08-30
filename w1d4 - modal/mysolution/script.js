a88c3ef5b5aab3de6a748a2ecfec68cc38c2fa79
Laise Bang Henriksen <31646151+LaiseBang@users.noreply.github.com>
2018-08-30 15:28:38 +0200

    solution modal window is working

---
+++ w1d4 - modal/mysolution/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", hideModal);
+document.querySelector("#products").addEventListener("click", showModal);
+
+function showModal() {
+  console.log("Show modal");
+
+  document.querySelector("#modal_container").style.opacity = "1";
+
+  document.querySelector("#modal_container").style.pointerEvents = "auto"; // slå mus til
+  document.querySelector("#closebutton").addEventListener("click", hideModal);
+}
+
+function hideModal() {
+  console.log("Hide modal");
+
+  document.querySelector("#modal_container").style.pointerEvents = "none"; // slå mus fra
+  document.querySelector("#modal_container").style.opacity = "0";
+}
