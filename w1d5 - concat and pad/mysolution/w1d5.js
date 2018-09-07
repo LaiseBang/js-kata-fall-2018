1a7cc898670f527ceaee904f1ada7b8c3a955a44
Laise Bang Henriksen <31646151+LaiseBang@users.noreply.github.com>
2018-09-07 21:25:30 +0200

    concat and pad sort of works

--- w1d5 - concat and pad/mysolution/w1d5.js
+++ w1d5 - concat and pad/mysolution/w1d5.js
@@ -1,55 +1,15 @@
 "use strict";
 
-//define variable
-const text = "image#";
-const hastag = /#/gi;
-const number = [];
+let newText;
 
-//call the function
-concatNPad();
-//make function init
-
-//make function concatNPad
-function concatNPad() {
-  //recive text string
-  //const hastag = text.substring(5);
-  console.log(text.replace(hastag, number));
+function concatNPad(name, number) {
+  if (number < 10) {
+    newText = name.replace("#", "0" + number);
+    return newText;
+  } else {
+    newText = name.replace("#", number);
+    return newText;
+  }
 }
-/*Make a function that receives a 'pattern'-string and a number.  
-It returns a string based on the pattern, with # replaced by the number, padded to atleast two digits.
-
-Examples:
-
-```javascript
-concatNPad("image#.jpg", 3);
-```
-
-would return `'image03.jpg'`
-
-Test the function directly in the console, call it with various patterns and numbers.
-
-#### Hints
-
-Use some or more of these String-methods:
-
-- `.indexOf`
-- `.substring`
-- `.replace`
-- `.padStart`
-- `.padEnd`
-
-#### Test
-
-Test the function with these inputs, and make sure you get the corresponding output:
-
-| Pattern           | Number | Output               |
-| ----------------- | -----: | -------------------- |
-| 'image#.jpg'      |      3 | 'image03.jpg'        |
-| 'image#.jpg'      |     12 | 'image12.jpg'        |
-| 'image#.jpg'      |    123 | 'image123.jpg'       |
-| '#\_chapter.txt'  |      1 | '01_chapter.txt'     |
-| '#\_chapter.txt'  |     10 | '10_chapter.txt'     |
-| 'myfile_backup.#' |      8 | 'myfile_backup.08'   |
-| 'myfile_backup.#' |     39 | 'myfile_backup.39'   |
-| 'myfile_backup.#' |   2018 | 'myfile_backup.2018' |
-*/
+console.log(concatNPad("image#.jpg", 3));
+console.log(concatNPad("image##.jpg", 4)); //this does not work
