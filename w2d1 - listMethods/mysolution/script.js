c0c502787d5a5dadba876d74439b9479a685915f
Laise Bang Henriksen <31646151+LaiseBang@users.noreply.github.com>
2018-09-08 09:34:53 +0200

    a solution to kata w2l1

--- w2d1 - listMethods/mysolution/script.js
+++ w2d1 - listMethods/mysolution/script.js
@@ -1,11 +1,48 @@
+/* 
+This is the resoult of my struggles, nothing works
+
 "use strict";
+let element;
+let findLi = [];
+//function displayElement(element) {}
 
-function displayElement( element ) {
-    console.log( element.textContent );
-}
-
-listMethods("modify");
-listMethods("info");
-listMethods("newarray");
+listMethods("modify>ul>li");
+//listMethods("info>ul>li");
+//listMethods("newarray>ul>li");
 
 // TODO: Create listMethods function
+function listMethods(findLi) {
+  element = document.querySelectorAll(findLi);
+  console.log(element);
+  //displayElement();
+} */
+
+// The following solution is made by canyouseethroughme
+"use strict";
+
+function listMethods(id) {
+  let lis = document.querySelectorAll("#" + id + " li");
+  lis.forEach(li => {
+    displayElement(li);
+  });
+}
+
+function listMethodsFor(id) {
+  const lis = document.querySelectorAll("#" + id + " li");
+  for (let i = 0; i < lis.length; i++) {
+    displayElement(lis[i]);
+  }
+}
+
+function displayElement(element) {
+  console.log(element.textContent);
+}
+
+listMethods("info");
+listMethods("modify");
+listMethods("newarray");
+
+console.log("=======");
+listMethodsFor("info");
+listMethodsFor("modify");
+listMethodsFor("newarray");
